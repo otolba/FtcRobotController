e3c2537be0500188ac76405d05202ab965ebe421
Torterra890 <mr.lucian.smith@gmail.com>
2023-12-08 15:27:42 -0800

    Updated all corner files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueFarCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueFarCorner.java
@@ -103,11 +103,11 @@ public class AutoBlueFarCorner extends RobotLinearOpMode
             case RIGHT:
             {
                 encoderDrive(0.3, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 6, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.5, 8, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 16, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 23, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 7, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 3, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 6, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.5, 21, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
                 sleep(5000);
                 encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_LEFT);
@@ -127,12 +127,13 @@ public class AutoBlueFarCorner extends RobotLinearOpMode
             case LEFT:
             {
                 /* Your autonomous code */
-                encoderDrive(0.5, 25, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 8.5, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 2, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 7, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 8, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 20, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
                 encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 sleep(25000);
@@ -141,11 +142,24 @@ public class AutoBlueFarCorner extends RobotLinearOpMode
             case CENTER:
             {
                 /* Your autonomous code*/
-                encoderDrive(0.5, 28, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 27.5, MOVEMENT_DIRECTION.FORWARD);
                 encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 2, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 encoderDrive(0.5, 24, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
+                encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                sleep(25000);
+            }
+            case RIGHT:
+            {
+                encoderDrive(0.5, 27.5, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 2, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
                 encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 sleep(25000);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFarCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedFarCorner.java
@@ -102,13 +102,14 @@ public class AutoRedFarCorner extends RobotLinearOpMode
         switch (snapshotAnalysis){
             case RIGHT:
             {
-                encoderDrive(0.3, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.3, 4, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 encoderDrive(0.5, 24, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 6, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 2, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.5, 6, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 21, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 7, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 8, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.5, 20, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
                 encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 sleep(25000);
@@ -131,12 +132,13 @@ public class AutoRedFarCorner extends RobotLinearOpMode
             case LEFT:
             {
                 /* Your autonomous code */
-                encoderDrive(0.5, 25, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 8.5, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
-                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 16, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 23, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 7, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                encoderDrive(0.5, 3, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 6, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 21, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
                 encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 sleep(25000);
@@ -145,15 +147,26 @@ public class AutoRedFarCorner extends RobotLinearOpMode
             case CENTER:
             {
                 /* Your autonomous code*/
-                encoderDrive(0.5, 28, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 27.5, MOVEMENT_DIRECTION.FORWARD);
                 encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 2, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 25, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.REVERSE);
                 encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                encoderDrive(0.5, 15, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                encoderDrive(0.5, 40, MOVEMENT_DIRECTION.FORWARD);
-                encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                sleep(5000);
+                encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                sleep(25000);
+            }
+            case RIGHT:
+            {
+                encoderDrive(0.5, 27.5, MOVEMENT_DIRECTION.FORWARD);
+                encoderDrive(0.5, 10, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 2, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 24, MOVEMENT_DIRECTION.REVERSE);
+                encoderDrive(0.5, 4, MOVEMENT_DIRECTION.FORWARD);
+                sleep(5000);
+                encoderDrive(0.5, 65, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                encoderDrive(0.5, 5, MOVEMENT_DIRECTION.STRAFE_LEFT);
                 sleep(25000);
             }
         }
