68b3b28a536cd52a2a36ab7bed4312364fce0007
Torterra890 <mr.lucian.smith@gmail.com>
2024-01-26 14:27:58 -0800

    aprilTag detection fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLinearOpMode.java
@@ -788,23 +788,27 @@ public abstract class RobotLinearOpMode extends LinearOpMode {
         telemetry.addData("# AprilTags Detected", currentDetections.size());
         boolean[] aprilTags = new boolean[7];
         // Step through the list of detections and display info for each one.
-        for (AprilTagDetection detection : currentDetections)
+        if (currentDetections.size() != 0)
         {
-            if (detection.metadata != null)
+            for (AprilTagDetection detection : currentDetections)
             {
-                aprilTags[detection.id] = true;
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-            }
-            else
-            {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
-            }
+                if (detection.metadata != null)
+                {
+                    aprilTags[detection.id] = true;
+                    telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                    telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                }
+                else
+                {
+                    telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                    telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                }
 
-        }   // end for() loop
+            }   // end for() loop
+        }
+        
         // Add "key" information to telemetry
         telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
         telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
