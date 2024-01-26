770ed17ae71899a254068ba430c42a9bc8c78ed9
Torterra890 <mr.lucian.smith@gmail.com>
2024-01-25 17:18:16 -0800

    Update RobotLinearOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotLinearOpMode.java
@@ -16,8 +16,17 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
-
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 /**
@@ -43,6 +52,9 @@ public abstract class RobotLinearOpMode extends LinearOpMode {
     NormalizedColorSensor colorSensor2;
     Servo placePurplePixel;
     Servo placeYellowServo;
+    AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
+    boolean USE_WEBCAM = false;  // true for webcam, false for phone camera
     boolean placingPixel = false;
     boolean searching;
 
@@ -715,6 +727,55 @@ public abstract class RobotLinearOpMode extends LinearOpMode {
         rightFrontDriveMotor.setDirection(DcMotorEx.Direction.REVERSE);
         rightBackDriveMotor.setDirection(DcMotorEx.Direction.REVERSE);
     }
+
+
+    private void initAprilTag() {
+
+        USE_WEBCAM = true;
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+    }
+
+    public boolean[] getAprilTags()
+    {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        boolean[] aprilTags = new boolean[6];
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections)
+        {
+            if (detection.metadata != null)
+            {
+                aprilTags[detection.id-1] = true;
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else
+            {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+        }   // end for() loop
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+        return aprilTags;
+    }// end method telemetryAprilTag()
+
+
+
 
     enum MOVEMENT_DIRECTION {
         STRAFE_LEFT,
