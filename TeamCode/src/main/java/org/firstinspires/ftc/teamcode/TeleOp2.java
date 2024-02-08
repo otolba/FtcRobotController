c6584235eb3bc98e72b47b73743d0fa128f3c6c1
Torterra890 <mr.lucian.smith@gmail.com>
2024-02-08 13:20:55 -0800

    fixed conflict

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -142,17 +142,15 @@ public class TeleOp2 extends LinearOpMode {
                 bWasPressed = false;
             }
 
-            if (slowLifter){
+            if (slowLifter)
+            {
                 intakePower = -0.5;
             }
-<<<<<<< HEAD
             leftFrontPower *=0.8;
             rightFrontPower *= 0.75;
             leftBackPower *= 0.95;
             rightBackPower *= 1;
-=======
 
->>>>>>> 6d74aefe9efece50827f9a7351bebb20af80c1a0
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
