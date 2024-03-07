85c11dbf976a7e5f0a4ab23f79e54c64b922e642
otolba <omar16tolba@gmail.com>
2024-03-07 13:45:51 -0800

    f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkRed.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 
-@Autonomous(name = "park blue", group = "linear autoMode")
-public class ParkBlue extends RobotLinearOpMode{
+@Autonomous(name = "park red", group = "linear autoMode")
+public class ParkRed extends RobotLinearOpMode{
     @Override
     public void runOpMode(){
         declareHardwareProperties();
@@ -20,42 +20,42 @@ public class ParkBlue extends RobotLinearOpMode{
             if (close){
                 if (parkMiddle){
                     encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
-                    encoderDrive(0.7, 30, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                    encoderDrive(0.7, 30, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 }
                 else{
                     encoderDrive(0.7, 2, MOVEMENT_DIRECTION.FORWARD);
-                    encoderDrive(0.7, 30, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                    encoderDrive(0.7, 30, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                 }
             }
             else{
                 if (parkMiddle){
                     if (far){
                         encoderDrive(0.7, 3, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_LEFT);
                         encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 50, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                        encoderDrive(0.7, 50, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                     }
                     else{
                         encoderDrive(0.7, 3, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 50, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                        encoderDrive(0.7, 50, MOVEMENT_DIRECTION.STRAFE_LEFT);
                         encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 20, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                        encoderDrive(0.7, 20, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                     }
                 }
                 else{
                     if (far){
                         encoderDrive(0.7, 3, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_RIGHT);
-                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.STRAFE_LEFT);
-                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.REVERSE);
                         encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
+                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                        encoderDrive(0.7, 40, MOVEMENT_DIRECTION.REVERSE);
+                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                     }
                     else{
                         encoderDrive(0.7, 3, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_RIGHT);
+                        encoderDrive(0.7, 15, MOVEMENT_DIRECTION.STRAFE_LEFT);
                         encoderDrive(0.7, 40, MOVEMENT_DIRECTION.FORWARD);
-                        encoderDrive(0.7, 60, MOVEMENT_DIRECTION.STRAFE_LEFT);
+                        encoderDrive(0.7, 60, MOVEMENT_DIRECTION.STRAFE_RIGHT);
                     }
                 }
             }
